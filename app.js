420ece21ec6f43f4ae9adefcee01d5363b57abce
ohwellmylove <maowens66@gmail.com>
2021-11-24 06:39:26 -0500

    added review delete functionality and campground cascading delete for reviews

--- app.js
+++ app.js
@@ -107,8 +107,8 @@ app.delete(
 );
 
 app.post(
-    '/campgrounds/:id/reviews',
-    validateReview,
+	'/campgrounds/:id/reviews',
+	validateReview,
 	catchAsync(async (req, res) => {
 		const campground = await Campground.findById(req.params.id);
 		const review = new Review(req.body.review);
@@ -116,6 +116,16 @@ app.post(
 		await review.save();
 		await campground.save();
 		res.redirect(`/campgrounds/${campground._id}`);
+	})
+);
+
+app.delete(
+	'/campgrounds/:id/reviews/:reviewId',
+	catchAsync(async (req, res) => {
+		const { id, reviewId } = req.params;
+		await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+		await Review.findByIdAndDelete(reviewId);
+		res.redirect(`/campgrounds/${id}`);
 	})
 );
 
